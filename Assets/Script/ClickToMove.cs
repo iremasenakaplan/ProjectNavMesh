5456a94f67dcfc040a20757b1f97cdc87998163c
iremasenakaplan <iremasenakaplan19@gmail.com>
2022-11-17 22:33:42 +0300

    Players, obstacle, move

---
+++ Assets/Script/ClickToMove.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))] //NavMesh componentinin gerek old. belirtiliyor
+
+public class ClickToMove : MonoBehaviour
+{
+    NavMeshAgent m_Agent;
+    RaycastHit m_HitInfo = new RaycastHit();
+
+    void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+           var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+           if(Physics.Raycast(ray.origin, ray.direction, out m_HitInfo))
+           {
+               m_Agent.destination = m_HitInfo.point;
+           }
+        }
+    }
+}
